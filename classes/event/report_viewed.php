1ad73fc8aae2c27a7df756f4fda8047c00fe9483
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2026-03-15 14:45:33 +1300

    Improved table footer styling

--- classes/event/report_viewed.php
+++ classes/event/report_viewed.php
@@ -31,7 +31,6 @@ namespace report_dashboard\event;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class report_viewed extends \core\event\base {
-
     /**
      * Init method.
      *
